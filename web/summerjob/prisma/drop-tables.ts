35e0ff911a6e357b3d30be63d15e41135f7d353d
Matyáš Ješina <jesinmat@fit.cvut.cz>
2023-01-06 14:16:22 +0100

    Add entities to database - jobs, plan, area

---
+++ web/summerjob/prisma/drop-tables.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "../lib/prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const tablenames = await prisma.$queryRaw<
+    Array<{ tablename: string }>
+  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+
+  const tables = tablenames
+    .map(({ tablename }) => tablename)
+    .filter((name) => name !== "_prisma_migrations")
+    .map((name) => `"public"."${name}"`)
+    .join(", ");
+
+  try {
+    console.log(`TRUNCATE TABLE ${tables} CASCADE;`);
+    await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`);
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- web/summerjob/prisma/seed.ts
+++ web/summerjob/prisma/seed.ts
@@ -1,4 +1,11 @@
-import { Allergy, PrismaClient } from "../lib/prisma/client";
+import {
+  Allergy,
+  Plan,
+  PrismaClient,
+  ProposedJob,
+  SummerJobEvent,
+  Worker,
+} from "../lib/prisma/client";
 import { faker } from "@faker-js/faker/locale/cz";
 
 const prisma = new PrismaClient();
@@ -38,6 +45,7 @@ async function createWorkers(allergies?: Allergy[]) {
       lastName: lastName,
       phone: faker.phone.number("### ### ###"),
       email: faker.internet.email(firstName, lastName),
+      isStrong: Math.random() > 0.75,
     };
   };
   const withCar = (worker: any) => {
@@ -53,6 +61,9 @@ async function createWorkers(allergies?: Allergy[]) {
   };
   for (let i = 0; i < 7; i++) {
     const worker = createWorker();
+    if (i === 0) {
+      worker.isStrong = true;
+    }
     await prisma.worker.create({
       data: worker,
     });
@@ -80,11 +91,110 @@ async function createWorkers(allergies?: Allergy[]) {
       });
     }
   }
+  return await prisma.worker.findMany();
+}
+
+async function createYearlyEvent() {
+  const event = await prisma.summerJobEvent.create({
+    data: {
+      name: "Krkonoše 2023",
+      startDate: new Date("2023-07-03"),
+      endDate: new Date("2023-07-07"),
+    },
+  });
+  return event;
+}
+
+async function createAreas(eventId: string) {
+  const createArea = () => {
+    return {
+      name: faker.address.city(),
+      description: faker.lorem.paragraph(),
+      summerJobEventId: eventId,
+    };
+  };
+  await prisma.area.createMany({
+    data: [createArea(), createArea(), createArea()],
+  });
+  return await prisma.area.findMany();
+}
+
+async function createProposedJobs(areaIds: string[]) {
+  const titles = [
+    "Hrabání listí",
+    "Přesouvání kamení",
+    "Řezání dřeva",
+    "Úprava zahrady",
+    "Vymalování místnosti",
+  ];
+  const createProposedJob = (name: string) => {
+    return {
+      name: name,
+      description: faker.lorem.paragraph(),
+      areaId: choose(areaIds, 1)[0],
+      requiredDays: between(1, 3),
+      minWorkers: between(2, 3),
+      maxWorkers: between(4, 6),
+      strongWorkers: between(0, 1),
+    };
+  };
+  await prisma.proposedJob.createMany({
+    data: titles.map((title) => createProposedJob(title)),
+  });
+  return await prisma.proposedJob.findMany();
+}
+
+async function createPlan(event: SummerJobEvent) {
+  const plan = await prisma.plan.create({
+    data: {
+      day: event.startDate,
+      summerJobEventId: event.id,
+    },
+  });
+  return plan;
+}
+
+async function populatePlan(
+  plan: Plan,
+  proposedJobs: ProposedJob[],
+  workers: Worker[]
+) {
+  const job = choose(proposedJobs, 1)[0];
+  const workersCount = between(job.minWorkers, job.maxWorkers);
+  const workersIds = choose(workers, workersCount).map((worker) => worker.id);
+  if (job.strongWorkers > 0) {
+    const strongWorker = workers.find((w) => w.isStrong) || workers[0];
+    if (!workersIds.includes(strongWorker.id)) {
+      workersIds[0] = strongWorker.id;
+    }
+  }
+  await prisma.activeJob.create({
+    data: {
+      privateDescription: "Popis úkolu, který vidí jen organizátor",
+      publicDescription: "Popis úkolu, který vidí všichni",
+      planId: plan.id,
+      jobId: job.id,
+      workers: {
+        connect: workersIds.map((id) => ({ id })),
+      },
+    },
+  });
 }
 
 async function main() {
   const allergies = await createAllergies();
-  await createWorkers(allergies);
+  console.log("Creating workers, cars...");
+  const workers = await createWorkers(allergies);
+  console.log("Creating yearly event...");
+  const yearlyEvent = await createYearlyEvent();
+  console.log("Creating areas...");
+  const areas = await createAreas(yearlyEvent.id);
+  console.log("Creating proposed jobs...");
+  const proposedJobs = await createProposedJobs(areas.map((area) => area.id));
+  console.log("Creating plan...");
+  const plan = await createPlan(yearlyEvent);
+  console.log("Populating plan...");
+  await populatePlan(plan, proposedJobs, workers);
 }
 
 main()
