6abbdb64d8821b2cd49b019e3bca88ef7f6ba728
Matyáš Ješina <jesinmat@fit.cvut.cz>
2023-04-04 16:12:34 +0200

    Permissions editor

---
+++ web/summerjob/lib/components/user/EditUserForm.tsx
@@ -0,0 +1,60 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useAPIUserUpdate } from "lib/fetcher/user";
+import { Permission } from "lib/types/auth";
+import { UserComplete, UserUpdateData, UserUpdateSchema } from "lib/types/user";
+import { useForm } from "react-hook-form";
+
+interface EditUserProps {
+  user: UserComplete;
+  onUpdate: () => void;
+}
+
+export default function EditUserForm({ user, onUpdate }: EditUserProps) {
+  const { trigger, error, isMutating, reset } = useAPIUserUpdate(user.id, {
+    onSuccess: () => onUpdate(),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserUpdateData>({
+    resolver: zodResolver(UserUpdateSchema),
+    defaultValues: {
+      permissions: user.permissions,
+    },
+  });
+
+  const onSubmit = (data: UserUpdateData) => {
+    trigger(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+      <div className="fs-5 mb-3">
+        {user.firstName} {user.lastName}
+      </div>
+      <p className="text-muted">
+        Uživatelé s rolí ADMIN mají automaticky přístup ke všem stránkám.
+      </p>
+      <div className="mb-3">
+        {Object.values(Permission).map((permission) => (
+          <label className="d-flex align-items-center gap-2">
+            <input
+              type="checkbox"
+              value={permission}
+              className="form-check-input fs-5 checkbox-white"
+              {...register("permissions")}
+            />{" "}
+            <span className="d-inline-block fs-5">{permission}</span>
+          </label>
+        ))}
+      </div>
+      <input
+        type={"submit"}
+        className="btn btn-warning float-end pt-2 pb-2"
+        value={"Uložit"}
+        disabled={isMutating}
+      />
+    </form>
+  );
+}
--- web/summerjob/lib/components/user/UserRow.tsx
+++ web/summerjob/lib/components/user/UserRow.tsx
@@ -3,7 +3,9 @@ import { useAPIUserUpdate } from "lib/fetcher/user";
 import { UserComplete } from "lib/types/user";
 import { useState } from "react";
 import ConfirmationModal from "../modal/ConfirmationModal";
+import { Modal, ModalSize } from "../modal/Modal";
 import { SimpleRow } from "../table/SimpleRow";
+import EditUserForm from "./EditUserForm";
 
 interface UserRowProps {
   user: UserComplete;
@@ -18,10 +20,14 @@ export default function UserRow({ user, onUpdate }: UserRowProps) {
     trigger({ blocked: !user.blocked });
   };
 
-  return <SimpleRow data={formatUserRow(user, toggleLocked)} />;
+  return <SimpleRow data={formatUserRow(user, toggleLocked, onUpdate)} />;
 }
 
-function formatUserRow(user: UserComplete, toggleLocked: () => void) {
+function formatUserRow(
+  user: UserComplete,
+  toggleLocked: () => void,
+  onUserEdited: () => void
+) {
   const permissions = user.permissions;
   // TODO: translate permissions
   const permissionString = permissions.join(", ");
@@ -33,10 +39,7 @@ function formatUserRow(user: UserComplete, toggleLocked: () => void) {
       key={`actions-${user.id}`}
       className="d-flex align-items-center gap-3"
     >
-      <i
-        className="fas fa-edit smj-action-edit cursor-pointer"
-        title="Upravit role"
-      ></i>
+      <EditIcon onEdited={onUserEdited} user={user} />
       <LockIcon locked={user.blocked} onConfirm={toggleLocked} />
     </span>,
   ];
@@ -91,6 +94,42 @@ function LockIcon({
             </>
           )}
         </ConfirmationModal>
+      )}
+    </>
+  );
+}
+
+function EditIcon({
+  user,
+  onEdited,
+}: {
+  user: UserComplete;
+  onEdited: () => void;
+}) {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const onDialogConfirmed = () => {
+    setDialogOpen(false);
+    onEdited();
+  };
+  const onDialogCancelled = () => {
+    setDialogOpen(false);
+  };
+  return (
+    <>
+      <i
+        className="fas fa-edit smj-action-edit cursor-pointer"
+        title="Upravit role"
+        onClick={() => setDialogOpen(true)}
+      />
+
+      {dialogOpen && (
+        <Modal
+          size={ModalSize.MEDIUM}
+          title="Upravit role"
+          onClose={onDialogCancelled}
+        >
+          <EditUserForm onUpdate={onDialogConfirmed} user={user} />
+        </Modal>
       )}
     </>
   );
