297b59802371e69abe49258be9b3599be3db11a5
Matyáš Ješina <jesinmat@fit.cvut.cz>
2023-04-04 16:54:04 +0200

    Add error message for perms saving

--- web/summerjob/lib/components/user/EditUserForm.tsx
+++ web/summerjob/lib/components/user/EditUserForm.tsx
@@ -3,6 +3,7 @@ import { useAPIUserUpdate } from "lib/fetcher/user";
 import { Permission } from "lib/types/auth";
 import { UserComplete, UserUpdateData, UserUpdateSchema } from "lib/types/user";
 import { useForm } from "react-hook-form";
+import ErrorMessageModal from "../modal/ErrorMessageModal";
 
 interface EditUserProps {
   user: UserComplete;
@@ -29,32 +30,35 @@ export default function EditUserForm({ user, onUpdate }: EditUserProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-      <div className="fs-5 mb-3">
-        {user.firstName} {user.lastName}
-      </div>
-      <p className="text-muted">
-        Uživatelé s rolí ADMIN mají automaticky přístup ke všem stránkám.
-      </p>
-      <div className="mb-3">
-        {Object.values(Permission).map((permission) => (
-          <label className="d-flex align-items-center gap-2">
-            <input
-              type="checkbox"
-              value={permission}
-              className="form-check-input fs-5 checkbox-white"
-              {...register("permissions")}
-            />{" "}
-            <span className="d-inline-block fs-5">{permission}</span>
-          </label>
-        ))}
-      </div>
-      <input
-        type={"submit"}
-        className="btn btn-warning float-end pt-2 pb-2"
-        value={"Uložit"}
-        disabled={isMutating}
-      />
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+        <div className="fs-5 mb-3">
+          {user.firstName} {user.lastName}
+        </div>
+        <p className="text-muted">
+          Uživatelé s rolí ADMIN mají automaticky přístup ke všem stránkám.
+        </p>
+        <div className="mb-3">
+          {Object.values(Permission).map((permission) => (
+            <label className="d-flex align-items-center gap-2">
+              <input
+                type="checkbox"
+                value={permission}
+                className="form-check-input fs-5 checkbox-white"
+                {...register("permissions")}
+              />{" "}
+              <span className="d-inline-block fs-5">{permission}</span>
+            </label>
+          ))}
+        </div>
+        <input
+          type={"submit"}
+          className="btn btn-warning float-end pt-2 pb-2"
+          value={"Uložit"}
+          disabled={isMutating}
+        />
+      </form>
+      {error && <ErrorMessageModal onClose={reset} />}
+    </>
   );
 }
