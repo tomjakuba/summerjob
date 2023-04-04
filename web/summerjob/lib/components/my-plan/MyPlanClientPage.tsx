82068eabba95a60a77949b6935a360462b0dc9a1
Matyáš Ješina <jesinmat@fit.cvut.cz>
2023-04-04 17:55:09 +0200

    Remove unused imports

--- web/summerjob/lib/components/my-plan/MyPlanClientPage.tsx
+++ web/summerjob/lib/components/my-plan/MyPlanClientPage.tsx
@@ -1,13 +1,7 @@
 "use client";
 import { useAPIMyPlans } from "lib/fetcher/my-plan";
-import { formatDateLong } from "lib/helpers/helpers";
-import { translateAllergies } from "lib/types/allergy";
 import { deserializeMyPlans, MyPlan } from "lib/types/my-plan";
 import { Serialized } from "lib/types/serialize";
-import { useMemo, useState } from "react";
-import SimpleDatePicker from "../date-picker/date-picker";
-import EditBox from "../forms/EditBox";
-import PageHeader from "../page-header/PageHeader";
 import MyPlanBrowser from "./MyPlanBrowser";
 import MyPlanEmpty from "./MyPlanEmpty";
 
