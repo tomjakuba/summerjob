1816265f88cab76ff51b6724898a447f02bda735
Matyáš Ješina <jesinmat@fit.cvut.cz>
2023-04-25 14:30:37 +0200

    Add note about sending dates to API

--- web/app/(web)/(team)/workers/new/page.tsx
+++ web/app/(web)/(team)/workers/new/page.tsx
@@ -2,6 +2,8 @@ import EditBox from "lib/components/forms/EditBox";
 import CreateWorker from "lib/components/worker/CreateWorker";
 import { cache_getActiveSummerJobEvent } from "lib/data/cache";
 
+export const dynamic = "force-dynamic";
+
 export default async function CreateWorkerPage() {
   const summerJobEvent = await cache_getActiveSummerJobEvent();
   const { startDate, endDate } = summerJobEvent!;
