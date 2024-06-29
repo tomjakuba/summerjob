9fdaae526c1a1893c6ce5d2f11ed213284ce6e1f
ladal1 <ladislav.louka@gmail.com>
2024-06-29 17:52:28 +0200

    Skill enum enhance

--- web/lib/data/enumMapping/skillMapping.ts
+++ web/lib/data/enumMapping/skillMapping.ts
@@ -2,10 +2,11 @@ import { Skill } from '../../prisma/client'
 import { EnumMapping } from './enumMapping'
 
 export const skillMapping: EnumMapping<keyof typeof Skill> = {
-  LUMBERJACK: 'Práce se sekerou',
-  ARTIST: 'Umělec',
-  GARDENER: 'Zahradník',
-  DANGER: 'Práce s nebezpečnými nástroji',
+  LUMBERJACK: 'Motorová pila',
+  ARTIST: 'Malování',
+  GARDENER: 'Křovinořez / Plotořez',
+  DANGER: 'Cirkulárka+fréza',
   ELECTRICIAN: 'Elektrikář',
   HEIGHTS: 'Práce ve výškách',
+  MASON: 'Zedník',
 }
