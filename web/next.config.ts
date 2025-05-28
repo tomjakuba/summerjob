660b1b37b4a93cb0cb2bc6303b5559c52d640ae0
ladal1 <ladislav.louka@gmail.com>
2025-05-28 12:15:35 +0200

    Refactor Next.js configuration and update dev script to remove turbopack

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'standalone',
+  distDir: 'build',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/my-plan',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
